cdb198e10830ca01a4894333a6d6abda0518332c
Jon Tyree <jontyree@gmail.com>
2025-06-23 14:56:29 -0500

    Update build.bat

--- build.bat
+++ build.bat
@@ -1,2 +1,5 @@
 call mvn clean install -DskipTests
 docker buildx build -t expense-app:1 .
+docker stop expense-app
+docker rm expense-app
+docker run -d -p 8080:8080 --name expense-app expense-app:1
